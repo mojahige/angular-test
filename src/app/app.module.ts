68f2bdd23d807e65bf50535050a0ff23c1f89cbd
Yu Nozato <nozato@j-yado.com>
2018-03-19 15:02:03 +0900

    Prettier formatted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/header/header.component";
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
