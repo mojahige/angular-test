6ec48863fa40c12a44581b9e1f4446b3bfb91e56
Yu Nozato <nozato@j-yado.com>
2018-03-22 14:23:46 +0900

    submit イベントまで
    value は取れてないんだなぁ

--- src/app/components/chat-input/chat-input.component.ts
+++ src/app/components/chat-input/chat-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-chat-input',
@@ -9,4 +10,8 @@ export class ChatInputComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onSubmit(f: NgForm) {
+    console.log(f)
+  }
 }
