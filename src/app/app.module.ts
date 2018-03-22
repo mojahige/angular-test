6b4608f7e5b8f95c5702d4a065c9382c52986a86
Yu Nozato <nozato@j-yado.com>
2018-03-22 13:59:45 +0900

    チャットの入力箇所作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
+import { ChatInputComponent } from './components/chat-input/chat-input.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChatInputComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
