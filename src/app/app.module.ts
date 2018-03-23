9a38d9a27a8e5a9a855ad52a11f4ec61ce3633b5
Yu Nozato <nozato@j-yado.com>
2018-03-23 13:46:33 +0900

    angular-material を使う

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms' // <-- NgModel lives here
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { ChatInputComponent } from './components/chat-input/chat-input.component'
 
-import { ChatService } from './services/chat.service';
+import { ChatService } from './services/chat.service'
 import { ChatMessageComponent } from './components/chat-message/chat-message.component'
 
 @NgModule({
@@ -18,7 +20,7 @@ import { ChatMessageComponent } from './components/chat-message/chat-message.com
     ChatInputComponent,
     ChatMessageComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [ChatService],
   bootstrap: [AppComponent]
 })
