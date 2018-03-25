4fa65fbcf8ad4a8041143c9831d1e9c0393ba4a8
YuNozato <mojahige@gmail.com>
2018-03-25 15:37:54 +0900

    ユーザー情報を持ちたいので service 追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { ChatService } from './services/chat.service'
+import { UserService } from './services/user.service'
+
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { ChatInputComponent } from './components/chat-input/chat-input.component'
-
-import { ChatService } from './services/chat.service'
 import { MessageBoardComponent } from './components/message-board/message-board.component'
 
 @NgModule({
